d0958df1dcbbd39d4ad00df7a09a8d0bb22aa1f0
Richard Ranke <richard.ranke@salesfusion.com>
2015-12-10 16:38:55 -0500

    simple styling

--- js/index.js
+++ js/index.js
@@ -27,43 +27,84 @@
 
     var Observable = Rx.Observable;
     var requestStream = Observable.just(URL);
-    var responseStream = requestStream.flatMap(function (url) {
-        return Observable.fromPromise(getJSON('GET', url))
-            .map(function(data) {
-                return data.headDocument.content
-                .filter(function (card) {
-                    return card.source === TWITTER;
-                })
-                .map(function(card) {
-                    return {
-                        id: card.collectionId,
-                        html: card.content.bodyHtml,
-                        attachments: card.content.attachments
-                    };
-                    // could update database here
-            });
-        });
-    });
 
+
+    var select = document.querySelector('select');
+    var selectAction = Observable.fromEvent(select, 'change');
+    selectAction.subscribe(function (event){
+        if(event.target.selectedIndex === 0) {
+            getSome(20);
+        } else if (event.target.selectedIndex === 1) {
+            console.log(';kj;k');
+            getSome(20);
+        } else if (event.target.selectedIndex === 2) {
+            console.log('woah');
+            getSome(19);
+        } else if (event.target.selectedIndex === 3) {
+            getSome(19);
+        }
+
+    });
     function updateUi (cards) {
         var el = document.querySelector('.container ul');
 
         cards.forEach(function(card) {
+
             var li = document.createElement('li');
-            var div = document.createElement('div');
+            var span = document.createElement('span');
             var img = document.createElement('img');
-            img.src = card.attachments[0].thumbnail_url;
-            div.innerHTML = card.html;
+            var ok = document.createElement('button');
+            var cancel = document.createElement('button');
+
+            ok.innerText = "Pass";
+            ok.classList.add('blue');
+            ok.classList.add('raised');
+            cancel.innerText = "Reject";
+            cancel.classList.add('gray');
+            cancel.classList.add('raised');
+            if (card.attachments !== undefined) {
+                img.src = card.attachments[0].thumbnail_url;
+            }
+
+            span.innerHTML = card.html;
 
             li.appendChild(img);
-            li.appendChild(div);
+            li.appendChild(span);
+            li.appendChild(ok);
+            li.appendChild(cancel);
             el.appendChild(li);
         });
     }
 
+    function getSome (social) {
+        console.log('get here?');
+        var responseStream = requestStream.flatMap(function (url) {
 
-    responseStream.subscribe(function (data) {
-        updateUi(data);
-        // could update database here
-    });
+            return Observable.fromPromise(getJSON('GET', url))
+                .map(function (data) {
+
+                    return data.headDocument.content
+                        .filter(function (cards) {
+                            return cards.source === social;
+                        })
+                        .map(function (card) {
+                            return {
+                                id: card.collectionId,
+                                html: card.content.bodyHtml,
+                                attachments: card.content.attachments
+                            };
+                            // could update database here
+                        }).filter(function (cards) {
+                            return cards.html !== undefined;
+                        });
+                });
+        });
+        responseStream.subscribe(function (data) {
+            updateUi(data);
+            // could update database here
+        });
+
+    }
+    getSome(20);
+
 })();
